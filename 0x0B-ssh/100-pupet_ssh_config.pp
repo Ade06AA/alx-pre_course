756ab9b87820f7a772038632a79a0725938bb1e9
Ade06AA <aburrahman.adenowo06@gmail.com>
2023-11-27 00:34:51 +0100

    #ssh

--- 0x0B-ssh/100-pupet_ssh_config.pp
+++ 0x0B-ssh/100-pupet_ssh_config.pp
@@ -3,7 +3,7 @@ include stdlib
 
 file { '/etc/ssh/ssh_config':
   ensure => present,
-}-> file_line { 'use private key in ~/.ssh/school':
+}-> file_line { 'Use private key in ~/.ssh/school':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
 }-> file_line { 'Disable password Authentication':
