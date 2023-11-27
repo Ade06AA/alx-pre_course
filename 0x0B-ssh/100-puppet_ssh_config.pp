f92c30a3b719d1c9c21671f392c7b482b647276a
Ade06AA <aburrahman.adenowo06@gmail.com>
2023-11-27 01:25:37 +0100

    #ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Change SSH configuration file
+
+exec { 'first_update':
+  command => 'echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  path          => '/usr/bin'
+}
+
+exec { 'second_update':
+  command => 'echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  path          => '/usr/bin'
+}
+
