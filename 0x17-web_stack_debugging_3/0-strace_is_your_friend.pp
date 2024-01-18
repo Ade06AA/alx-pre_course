b559bb2a3dfafbc306e474980a1876b38ee61b27
Ade06AA <aburrahman.adenowo06@gmail.com>
2024-01-18 02:05:25 +0100

    new

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# debugging
+
+exec { "bug":
+        command   => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+        path               => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+}
+
